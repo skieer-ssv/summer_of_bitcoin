ce772d4824e20451ec2fd874496648d9cfb1c8e5
Siddhant <55557885+skieer-ssv@users.noreply.github.com>
2021-06-15 12:27:40 +0530

    updated comments

--- solution.py
+++ solution.py
@@ -18,6 +18,7 @@ visited = {}
 tmap = {}
 
 '''
+#TODO: Make this function iterative instead of recursive
 def knapsack(tlist, n, w):
     global tmap
     print(n)
@@ -57,8 +58,8 @@ def knapsack(tlist, n, w):
 def effmap(tid):
     global tmap
     if tmap[tid][3] != -1:  # check if effective has been calculated
-        return tmap[tid][3:7]
-    w, f = 0, 0  # initialise weight n fees
+        return tmap[tid][3:6] #return effective fees weight and parents
+    w, f = 0, 0  # initialise weight and fees
     p = []  # initialise parent list
     for j in tmap[tid][2]:  # iterating through parents
         if j != '':
@@ -69,14 +70,16 @@ def effmap(tid):
     f += int(tmap[tid][0])
     w += int(tmap[tid][1])
     p.extend(tmap[tid][2])
-    tmap[tid][3] = f
-    tmap[tid][4] = w
-    tmap[tid][5] = p
+    tmap[tid][3] = f #fees
+    tmap[tid][4] = w #weight
+    tmap[tid][5] = p #parents
     tmap[tid][6]=f/w # effective fees per 1 wt
     return[f, w, p]
 
 
 def main():
+	
+	#Parsing the csv file
     filename = "mempool.csv"
     global tmap, cost_limit
     fields = []
@@ -90,33 +93,35 @@ def main():
         for row in csvreader:
             tmap[row[0]] = list(map(int, row[1:3]))
             tmap[row[0]].append(row[3].split(';'))
-            tmap[row[0]].extend([-1, -1, [],-1])
+            tmap[row[0]].extend([-1, -1, [],-1]) #fees, weight, parents, effective fees per weight
+	
+#Calculating Efficiency
     for i in tmap.keys():
         effmap(i)
-
-    tmap = dict(sorted(tmap.items(), key=lambda item: item[1][6],reverse=True)) # sorting with the effective fees per wt
-
+# sorting with the effective fees per wt
+    tmap = dict(sorted(tmap.items(), key=lambda item: item[1][6],reverse=True)) 
     tmap_og = tmap.copy()
     max_tid = []
     l = list(tmap.keys())
     
+#call for the knapsack function 
     # z = knapsack(l, len(l)-1, cost_limit)
-    
+#     print(z[1])
     for t in l:
         if cost_limit<1:
             break
         if t in tmap.keys():
-            if int(tmap[t][1]) > cost_limit:
+            if int(tmap[t][1]) > cost_limit: # if fees > cost_limit
                 continue
-            cost_limit -= int(tmap[t][1])
+            cost_limit -= int(tmap[t][1]) # subtract fees from the cost_limit
             for i in tmap[t][2][::-1]: #reverse so that the oldest parents always come first
-                if i != '':
+                if i != '': #if the list of parents does not contain an empty string
                     max_tid.append(i)
                     parent = tmap.pop(i, None) # remove the parent after its considered
-                    if parent == None:
-                        cost_limit += tmap_og[i][1] # parent of 2 transactions will only be considered once
+                    if parent == None: # None would signify that it has already been removed earlier
+                        cost_limit += tmap_og[i][1] # fees of parent of 2 transactions will only be considered once
                     else:
-                        l.remove(i)
+                        l.remove(i) # remove parent from the iteration list as it has been considered
             max_tid.append(t)
     
     f=open("block.txt","w")
